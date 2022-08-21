ff3b58ab3cfc6b2fa8ed8c9ac9f8c657b6e5bbac
Roman Hossain Shaon <roman.shaon@gmail.com>
2022-08-21 18:57:43 +0600

    queue uses notification for live mail sending notify_Notification_facade branch database

--- app/Notifications/EmailNotification.php
+++ app/Notifications/EmailNotification.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class EmailNotification extends Notification
+class EmailNotification extends Notification implements ShouldQueue
 {
     use Queueable;
 
