e6844243c8ee6b73aa3c699466748b3efe474fe5
Roman Hossain Shaon <roman.shaon@gmail.com>
2022-08-26 00:50:36 +0600

    Customer model might be associated with one Mobile model

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Customer extends Model
 {
     use HasFactory;
+
+    public function moblie()
+    {
+        return $this->hasOne(Mobile::class);
+    }
 }
